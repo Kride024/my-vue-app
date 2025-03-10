e42095b2cd5cfc111ddb866c99c7db2c631064aa
Kride024 <kritiyadavcoding@gmail.com>
2025-03-10 22:28:08 +0530

    Commit

--- showAddToCartCards.js
+++ showAddToCartCards.js
@@ -2,11 +2,41 @@
  import { getCartProductFromLS } from "./getCartProductFromLS";
 
  let cartProducts = getCartProductFromLS();
-
+//In filterProduct all value of LS stored
  let filterProducts = products.filter((curProd)=>{
   return cartProducts.some((curElem)=> curElem.id == curProd.id);
 // using some we get only element which are equal 
 });
 
  console.log(filterProducts);
+
+ //---------------------------
+ // to update the addToCart page
+ //------------------------------
+
+ const cartElement = document.querySelector("#productCartContainer");
+ const templateContainer = document.querySelector("#productCartTemplate");
+
+ const showCartProduct = () =>{
+    filterProducts.forEach((curProd)=>{
+        const {  category, id, image, name, stock,price} = curProd;
+        let productClone = document.importNode(templateContainer.content, true);
+
+        productClone.querySelector(".category").textContent = category;
+
+
+        cartElement.appendChild(productClone);
+    });
+
+ };
+
+ //----------------------------
+ // showing the cartProducts
+ //-----------------------------
+
+ showCartProduct();
+
+
+
+
  
