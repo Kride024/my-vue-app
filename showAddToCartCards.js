f137f4bc048951ec66724d4a7f7f6f7f08e59208
Kride024 <kritiyadavcoding@gmail.com>
2025-03-11 00:02:06 +0530

    Commit

--- showAddToCartCards.js
+++ showAddToCartCards.js
@@ -4,7 +4,7 @@
  let cartProducts = getCartProductFromLS();
 //In filterProduct all value of LS stored
  let filterProducts = products.filter((curProd)=>{
-  return cartProducts.some((curElem)=> curElem.id == curProd.id);
+ return cartProducts.some((curElem)=> curElem.id == curProd.id);
 // using some we get only element which are equal 
 });
 
@@ -21,9 +21,14 @@
     filterProducts.forEach((curProd)=>{
         const {  category, id, image, name, stock,price} = curProd;
         let productClone = document.importNode(templateContainer.content, true);
+          
+       const lSActualData = fetchQuantityFromCartLS(id,price);
 
+        productClone.querySelector("#cardValue").setAttribute("id",`card${id}`);
         productClone.querySelector(".category").textContent = category;
-
+        productClone.querySelector(".productName").textContent = name;
+        productClone.querySelector(".productImage").src=image;
+        
 
         cartElement.appendChild(productClone);
     });
